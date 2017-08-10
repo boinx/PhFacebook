846949cd11889b7577deeccafe74579c8b915e15
mb87.apple <mb87.apple@gmail.com>
2017-08-10 11:14:18 +0200

    Refs #4167: Bugfix - fixed missing PhAuthenticationToken public access

---
+++ PhFacebookHeaders.h
@@ -0,0 +1,16 @@
+//
+//  PhFacebookHeaders.h
+//  PhFacebook
+//
+//  Created by Mladen Bekavac on 09/08/2017.
+//
+//
+
+#ifndef PhFacebookHeaders_h
+#define PhFacebookHeaders_h
+
+#import "PhFacebook.h"
+#import "PhWebViewController.h"
+#import "PhAuthenticationToken.h"
+
+#endif /* PhFacebookHeaders_h */
